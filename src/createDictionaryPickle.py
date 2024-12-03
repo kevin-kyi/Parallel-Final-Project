55a6fd9d847c12e3cd3a6de2659099c2de4d14e6
Kevin Kyi <kev_kyi@Kevins-MacBook-Pro-9.local>
2024-12-02 19:07:35 -0500

    framework for creating kmeans dictionary and outline for creating pkl for training name/label data

---
+++ src/createDictionaryPickle.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+# This Function Will Create a .pkl file that has data about each image such as name and label 
+
+def createDictionaryPickle(directory, training_split):
+    print("Function used to create PKL for dictionary training")
+
+    return None
+
+
+
+
+
+
